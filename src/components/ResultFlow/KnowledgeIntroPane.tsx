bc3e34e6aeee84a82a279a0b2853983f7c5a6388
vinchaudinc <founders@letsblushh.com>
2025-01-08 22:15:09 +0700

    Update KnowledgeIntroPane.tsx
    text update

--- src/components/ResultFlow/KnowledgeIntroPane.tsx
+++ src/components/ResultFlow/KnowledgeIntroPane.tsx
@@ -9,15 +9,15 @@ export function KnowledgeIntroPane({ onNext }: { onNext: () => void }) {
           { type: "emoji", emoji: "💡" },
           {
             type: "title",
-            text: "Why Is It So Important to Learn About Binge Eating?",
+            text: "Why is it So Important to learn about binge eating?",
           },
           {
             type: "text",
-            text: "Building knowledge about binge eating helps us understand its causes, break the stigma, and find better ways to prevent it and recover..",
+            text: "Building knowledge about binge eating helps us understand its causes, break the stigma, and find better ways to prevent it and recover.",
           },
           {
             type: "text",
-            text: "It empowers us to foster a healthier relationship with food and care for ourselves.",
+            text: "It empowers us to foster a healthier relationship with food and care for ourselves!",
           },
         ],
       }}
