b3837abdcd506a1294ed4467dcf812fb0cbbbb96
vinchaudinc <founders@letsblushh.com>
2025-01-08 22:12:03 +0700

    Update TrialExplanation1.tsx
    trial -> 200% money back

--- src/components/Paywall/TrialExplanation1.tsx
+++ src/components/Paywall/TrialExplanation1.tsx
@@ -17,26 +17,21 @@ export function TrialExplanation1({ onNext }: { onNext: () => void }) {
       <TrialContents>
         <header>
           <Emoji>{"💡"}</Emoji>
-          <H2>{"Try R.care for 7 days to see if it works for you."}</H2>
+          <H2>{"See results or 200% money back."}</H2>
         </header>
         <Highlight>
           {
-            "Join thousands who have overcome binge eating & transformed their lives."
+            "Join thousands who've overcome binge eating & transformed their lives."
           }
         </Highlight>
         <List>
           <ListItem>
             {
-              "Read all our bitesized learning modules on the psychology and nutrition behind binge eating behavior."
+              "If you go through the R.care program for 30 days and don't see results, we offer 200% money back. "
             }
           </ListItem>
           <ListItem>
-            {"Use proven psychological techniques to stop binge eating."}
-          </ListItem>
-          <ListItem>
-            {
-              "Cultivate a balanced relationship with food with daily tasks and exercises."
-            }
+            {"Your success is our success. We're here to help you lead a healthier life, and we're confident you will see the difference."}
           </ListItem>
         </List>
       </TrialContents>
