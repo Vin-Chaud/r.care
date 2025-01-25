6ff3be4ebe0d937cc7cd6be9e7e915194ecad43e
Soul Codes <reach.soul.codes@gmail.com>
2025-01-25 08:28:06 -0500

    diagnose /result page issues

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -9,34 +9,43 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function ResultServer() {
+  console.log("1");
   const flow = defaultOnboardingFlow;
+  console.log("2");
   const onboardingSessionId = new ReadonlySession(cookies).getSessionifExists();
   if (!onboardingSessionId) {
     redirect("/");
   }
+  console.log("3");
 
   const onboardingDoc = await db
     .doc([config.firebase.collectionPath, onboardingSessionId].join("/"))
     .get();
+  console.log("4");
 
   if (!onboardingDoc.exists) {
     redirect("/");
   }
+  console.log("5");
 
   const data = onboardingDoc.data();
   if (!data?.main_quiz_done || !data?.quiz_data) {
     redirect("/quiz");
   }
+  console.log("6");
 
   if (data.checkout != null) {
     redirect("/onboarding_complete");
   }
+  console.log("7");
 
   if (data.quiz_data?.[flow.popup_quiz_step.id] == null) {
     redirect("/analysis");
   }
+  console.log("8");
 
   const imageUrls = await getGraphicImageUrls(flow, GraphicSection.Result);
+  console.log("9");
   return (
     <ResultClient
       flow={flow}
