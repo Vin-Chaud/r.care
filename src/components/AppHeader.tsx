eb8f3392092c23b0fc504e18dde294c904ef4a12
vinchaudinc <founders@letsblushh.com>
2025-10-08 21:59:06 +0900

    Update AppHeader.tsx

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -18,7 +18,7 @@ export function AppHeader({
       {typeof children === "string" ? (
         <Title>{children}</Title>
       ) : (
-        <RCareBrand height={14} />
+        <RCareBrand height={08} />
       )}
     </AppHeaderLayout>
   );
