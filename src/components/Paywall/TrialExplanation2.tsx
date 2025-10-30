25b251f4d442d6c7d2c1329205d06f77de30c1c6
vinchaudinc <founders@letsblushh.com>
2025-10-31 01:34:11 +0900

    Update user reference from Glint to Winmate

--- src/components/Paywall/TrialExplanation2.tsx
+++ src/components/Paywall/TrialExplanation2.tsx
@@ -26,7 +26,7 @@ export function TrialExplanation2({ onNext }: { onNext: () => void }) {
           <ListItem>Professionally crafted profiles can increase your match rate by up to 70%.</ListItem>
           <ListItem>Using tailored messages can get 30–60% more replies.</ListItem>
           <ListItem>8 out of 10 people who get coaching feel more confident and better about themselves.</ListItem>
-          <ListItem>In just one month, 70% of Glint users found dates they were genuinely satisfied with.</ListItem>
+          <ListItem>In just one month, 70% of Winmate users found dates they were genuinely satisfied with.</ListItem>
 
         </List>
       </TrialContents>
