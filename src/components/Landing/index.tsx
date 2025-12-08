b72d7e5c91a2f051907997f1967e8fa8be88208c
vinchaudinc <founders@letsblushh.com>
2025-12-08 17:42:24 +0900

    Update landing header text for clarity

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -34,7 +34,7 @@ export function Landing({ flow }: { flow: OnboardingFlow }) {
         <RCareBrand />
         <LandingHeaderLayout>
           <LandingHeader>
-           {"Online dating,\n Decoded."}
+           {"A sweet gentle space for your heart to grow"}
           </LandingHeader>
         </LandingHeaderLayout>
         <LandingQuiz
