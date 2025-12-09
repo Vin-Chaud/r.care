f1e48f6227d930095b99b6bc3d89be778d65f29b
vinchaudinc <founders@letsblushh.com>
2025-12-09 23:04:52 +0900

    Update index.tsx

--- src/components/ResultFlow/QuizResultPage/index.tsx
+++ src/components/ResultFlow/QuizResultPage/index.tsx
@@ -38,7 +38,14 @@ export function QuizResultPage({
           <ImpactBreakdown percentageScores={percentageScores} />
           <SymptomExplanations percentageScores={percentageScores} />
         </ScrollablePageContentFrame>
-      
+        <ScrollablePageContentFrame background={Greys.GreyDF}>
+          <ReactionPanel
+            onDidRespond={(answer) => {
+              onNext(answer);
+            }}
+          />
+        </ScrollablePageContentFrame>
+      </ScrollablePageLayoutContainer>
     </Fragment>
   );
 }
