5d7fdfd0f588346bad43701af9dd5cb0c1bece97
vinchaudinc <founders@letsblushh.com>
2025-12-10 01:02:08 +0900

    Remove TrialExplanation2 from Paywall component

--- src/components/Paywall/index.tsx
+++ src/components/Paywall/index.tsx
@@ -4,12 +4,10 @@ import { SubscriptionType } from "@/models/Subscription";
 import { useState } from "react";
 import { PaywallMain } from "./PaywallMain";
 import { TrialExplanation1 } from "./TrialExplanation1";
-import { TrialExplanation2 } from "./TrialExplanation2";
 import { TrialExplanation3 } from "./TrialExplanation3";
 
 enum Page {
   TrialExplanation1,
-  TrialExplanation2,
   TrialExplanation3,
   PaywallMain,
 }
@@ -22,16 +20,11 @@ export function Paywall({
   const [page, setPage] = useState(
     existingCartType ? Page.PaywallMain : Page.TrialExplanation1
   );
+
   switch (page) {
     case Page.TrialExplanation1: {
       return (
-        <TrialExplanation1 onNext={() => setPage(Page.TrialExplanation2)} />
-      );
-    }
-
-    case Page.TrialExplanation2: {
-      return (
-        <TrialExplanation2 onNext={() => setPage(Page.TrialExplanation3)} />
+        <TrialExplanation1 onNext={() => setPage(Page.TrialExplanation3)} />
       );
     }
 
