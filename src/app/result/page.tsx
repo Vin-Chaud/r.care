a970a6bcf340f4fa4d55cc9ff2030cfb9acd16b3
vinchaudinc <founders@letsblushh.com>
2026-01-14 15:21:21 +0900

    Refactor ResultServer function and clean up imports
    Removed unused imports and simplified result handling logic.

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,15 +1,10 @@
-import { ResultClient } from "@/app/result/ResultClient";
-import { getGraphicImageUrls } from "@/app/utils";
 import { config } from "@/config";
-import { defaultOnboardingFlow } from "@/models/default_flow";
-import { GraphicSection } from "@/models/OnboardingFlow/getGraphics";
 import { db } from "@/services/firebase";
 import { ReadonlySession } from "@/services/session";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function ResultServer() {
-  const flow = defaultOnboardingFlow;
   const onboardingSessionId = new ReadonlySession(cookies).getSessionifExists();
   if (!onboardingSessionId) {
     redirect("/");
@@ -32,16 +27,5 @@ export default async function ResultServer() {
     redirect("/onboarding_complete");
   }
 
-  if (data.quiz_data?.[flow.popup_quiz_step.id] == null) {
-    redirect("/analysis");
-  }
-
-  const imageUrls = await getGraphicImageUrls(flow, GraphicSection.Result);
-  return (
-    <ResultClient
-      flow={flow}
-      imageUrls={imageUrls}
-      responses={data.quiz_data}
-    />
-  );
+redirect("/paywall");
 }
