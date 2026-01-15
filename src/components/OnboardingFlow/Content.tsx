ee6630aebc8eefd943bb76eba0f3a4adecbe6be6
vinchaudinc <founders@letsblushh.com>
2026-01-15 15:12:31 +0900

    Update Content.tsx

--- src/components/OnboardingFlow/Content.tsx
+++ src/components/OnboardingFlow/Content.tsx
@@ -80,6 +80,26 @@ hasFullSpaceImage,
   }
 }
 
+function Image({ graphic_id, max_height, full_screen, style }: Content_Image)
+  {
+    const imageUrls = useOnboardingFlowImageUrls();
+    if (full_screen) {
+      return (
+        <>
+          <FullScreenImage src={imageUrls[graphic_id]} />
+          <FullScreenImageSeparator />
+        </>
+      );
+    }
+    return (
+      <img
+        src={imageUrls[graphic_id]}
+        style={{ maxHeight: max_height, maxWidth: "100%", ...(style || {}) }}
+      />
+    );
+  }
+
+
 function normalizeText(text: string | readonly string[]) {
   return typeof text === "string" ? text : text.join("<br>");
 }
@@ -95,6 +115,67 @@ const TextNormal = createRichText(styled.p`
   font-size: 17px;
   color: ${Greys.Grey26};
   margin-block: 20px;
+  text-align: center;
+`);
+const TextNormalDark = createRichText(styled.p`
+  ${Fonts.Inter}
+  font-size: 17px;
+  line-height: 23px;
+  color: ${Greys.GreyAF};
+  margin-block: 15px;
+  text-align: center;
+`);
+
+const TextNormalInFullScreenBackgroundContent = createRichText(styled.p`
+  ${Fonts.Inter}
+  font-size: 17px;
+  line-height: 20px;
+  margin-block: 15px;
+  text-align: center;
+`);
+
+const TextNormalInFullScreenBackgroundContentDark = createRichText(styled.p`
+  ${Fonts.Inter}
+  font-size: 17px;
+  line-height: 20px;
+  color: ${Greys.GreyAF};
+  margin-block: 15px;
+  text-align: center;
+`);
+
+const TextHighlight = createRichText(styled.p`
+  ${Fonts.Montserrat}
+  font-weight: 500;
+  font-size: 20px;
+  color: #7836cd;
+  margin-block: 15px;
+  text-align: center;
+`);
+
+const TextHighlightDark = createRichText(styled.p`
+  ${Fonts.Montserrat}
+  font-weight: 500;
+  font-size: 20px;
+  color: ${Purples.PurpleCA_Undocumented};
+  margin-block: 15px;
+  text-align: center;
+`);
+
+const TextHighlightInFullScreenBackgroundContent = createRichText(styled.p`
+  ${Fonts.Montserrat}
+  font-weight: 500;
+  font-size: 18px;
+  color: #7836cd;
+  margin-block: 15px;
+  text-align: center;
+`);
+
+const TextHighlightInFullScreenBackgroundContentDark = createRichText(styled.p`
+  ${Fonts.Montserrat}
+  font-weight: 500;
+  font-size: 18px;
+  color: ${Purples.PurpleCA_Undocumented};
+  margin-block: 15px;
   text-align: center;
 `);
 
@@ -114,7 +195,63 @@ const Title = createRichText(styled.h2`
   text-align: center;
 `);
 
+const TitleDark = createRichText(styled.h2`
+  ${Fonts.Inter}
+  color: ${Greys.GreyDF};
+  font-size: 24px;
+  font-weight: 600;
+  margin-block: 20px;
+  text-align: center;
+`);
+
+
+export const TitleInFullScreenBackgroundContent = createRichText(styled.h2`
+  ${Fonts.Inter}
+  font-size: 24px;
+  font-weight: 600;
+  margin-block: 10px;
+  text-align: center;
+`);
+
+const TitleInFullScreenBackgroundContentDark = createRichText(styled.h2`
+  ${Fonts.Inter}
+  color: ${Greys.GreyDF};
+  font-size: 24px;
+  font-weight: 600;
+  margin-block: 10px;
+  text-align: center;
+`);
+
+
 const TestimonialContainer = styled.div`
   padding-top: 40px;
   padding-bottom: 0px;
+`;
+
+export const FullScreenImage = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "src",
+})<{ src: string }>`
+  position: fixed;
+  display: block;
+  pointer-events: none;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#dacdff99, #dacdff99),
+    url(${(props) => props.src});
+  background-size: cover;
+  background-position: center;
+  z-index: -10;
+
+  @media (min-width: 420px) {
+    display: none;
+  }
+`;
+
+const FullScreenImageSeparator = styled.div`
+  flex-grow: 1;
+  @media (min-width: 420px) {
+    display: none;
+  }
 `;
