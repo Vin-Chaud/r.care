ff945bdd68a0c62984c27fb8119923bb1e834dd4
vinchaudinc <founders@letsblushh.com>
2026-01-16 11:37:29 +0900

    Update InfoStep.tsx

--- src/components/OnboardingFlow/InfoStep.tsx
+++ src/components/OnboardingFlow/InfoStep.tsx
@@ -10,31 +10,32 @@ import { PageLayout } from "@/design_components/PageLayout";
 import { fadeIn } from "@/utils/style_partials";
 
 export function InfoStep({ stepDefinition }: { stepDefinition: InfoScreen }) {
-     const { next } = useContext(onboardingFlowContext);
-      const { background, style, content_style, footer_style } = stepDefinition;
+ const { next } = useContext(onboardingFlowContext);
   const hasFullScreenImage = stepDefinition.contents.some(
     (content) => content.type === "image" && content.full_screen
   );
-    return (
-      <PageLayout background={Purples.PurpleF5_Undocumented} scrollable>
-         <InfoLayout style={style}>
-            <InfoContent style={content_style}>
+  return (
+    <PageLayout background={"#E5DEFA"}>
+      <InfoLayout>
+        <InfoHeader>
+          <RCareBrand height={14} />
+        </InfoHeader>
+        <InfoContent>
           {stepDefinition.contents.map((content, index) => (
             <Content
               key={index}
               content={content}
-              dark={stepDefinition.variant == "dark"}
               hasFullSpaceImage={hasFullScreenImage}
             />
           ))}
         </InfoContent>
-          <InfoFooter>
-            <ForwardNavButton onClick={next} />
-          </InfoFooter>
-        </InfoLayout>
-      </PageLayout>
-    );
-  }
+        <InfoFooter>
+          <ForwardNavButton onClick={next} />
+        </InfoFooter>
+      </InfoLayout>
+    </PageLayout>
+  );
+}
 
 
 
